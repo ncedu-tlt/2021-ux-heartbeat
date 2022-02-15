d553963e7f9b95fbe7a39adcd1b51d0e09a6b4da
Alexander Ionov <orlicus@gmail.com>
2022-02-15 13:32:28 +0400

    fix: Proper process.env typings

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,5 @@
-declare let process: {
-  env: {
+declare namespace NodeJS {
+  export interface ProcessEnv {
     NG_APP_ENV: string;
-    // Replace the line below with your environment variable for better type checking
-    [key: string]: any;
-  };
-};
+  }
+}
