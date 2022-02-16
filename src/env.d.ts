696a34e2c08f4aeb6764609adb1f76eb6831d677
AntoninaTrapsh <antrapsh@gmail.com>
2022-02-16 23:54:29 +0400

    fix/2-auth: env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,5 @@
 declare namespace NodeJS {
-  interface ProcessEnv {
+  export interface ProcessEnv {
     NG_APP_ENV: string;
     NG_APP_SUPABASE_URL: string;
     NG_APP_SUPABASE_ANON_KEY: string;
