c22e483506075a8ca230fa645234937dccbc461b
AntoninaTrapsh <antrapsh@gmail.com>
2022-02-20 19:16:27 +0400

    fix/9-player: fixed pipe

--- src/app/pipes/time-format.pipe.ts
+++ src/app/pipes/time-format.pipe.ts
@@ -4,9 +4,13 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "timeFormat"
 })
 export class FormatPipe implements PipeTransform {
-  transform(value: number): string {
-    const convertedTime: number | string =
-      value < 10 ? "0" + String(value) : value;
-    return `00:${convertedTime}`;
+  transform(value: number | null): string {
+    if (value === null) {
+      return " ";
+    } else {
+      return `00:${
+        +(value as string | number) < 10 ? "0" + String(value) : value
+      }`;
+    }
   }
 }
