a8bd81ecf3a52cb14a065851da98af8a0d377282
AntoninaTrapsh <antrapsh@gmail.com>
2022-03-01 23:08:36 +0400

    fix/12-tracklist: fix after 1st review

--- src/app/components/track/track/track.component.ts
+++ src/app/components/track/track/track.component.ts
@@ -1,39 +1,39 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { PlayerService } from "../../../services/player.service";
 import { PlayerTrackInfoModel } from "../../../models/player-track-info.model";
-import { combineLatest, tap } from "rxjs";
+import { combineLatest, Subscription } from "rxjs";
 
 @Component({
   selector: "hb-track",
   templateUrl: "./track.component.html",
   styleUrls: ["./track.component.less"]
 })
-export class TrackComponent implements OnInit {
-  trackTime = 30;
-  trackInfo!: PlayerTrackInfoModel;
+export class TrackComponent implements OnInit, OnDestroy {
+  public trackTime = 30;
+  public trackInfo!: PlayerTrackInfoModel;
+  private controlActiveTrack: Subscription = new Subscription();
+  public isPlay = false;
 
   @Input() public track!: PlayerTrackInfoModel;
-
-  isPlay = false;
 
   constructor(public playerService: PlayerService) {}
 
   ngOnInit() {
     this.trackInfo = this.track;
-    combineLatest(
+    this.controlActiveTrack = combineLatest(
       this.playerService.currentTrackInfo,
       this.playerService.isPlay
-    )
-      .pipe(
-        tap(([currentTrack, isPlay]) => {
-          if (currentTrack?.trackName === this.trackInfo.trackName) {
-            this.isPlay = isPlay;
-          } else {
-            this.isPlay = false;
-          }
-        })
-      )
-      .subscribe();
+    ).subscribe(([currentTrack, isPlay]) => {
+      if (currentTrack?.trackName === this.trackInfo.trackName) {
+        this.isPlay = isPlay;
+      } else {
+        this.isPlay = false;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.controlActiveTrack.unsubscribe();
   }
 
   controlPlayerCurrentTrack() {
