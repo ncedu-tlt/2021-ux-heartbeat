a432624c50e1b03e3aba15b3ae2cd1302000447d
AntoninaTrapsh <antrapsh@gmail.com>
2022-03-10 18:27:05 +0400

    fix after 1st review

--- src/app/pages/followed-artist-page/followed-artist-page.component.ts
+++ src/app/pages/followed-artist-page/followed-artist-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { ApiService } from "../../services/api.service";
 import { FollowedArtistModel } from "../../models/new-api-models/followed-artist.model";
 import { ArtistByIdModel } from "../../models/new-api-models/artist-by-id.model";
-import { BehaviorSubject } from "rxjs";
 
 @Component({
   selector: "hb-followed-artist-page",
@@ -10,19 +9,16 @@ import { BehaviorSubject } from "rxjs";
   styleUrls: ["./followed-artist-page.component.less"]
 })
 export class FollowedArtistPageComponent implements OnInit {
-  public artistInfo$ = new BehaviorSubject<ArtistByIdModel[] | null>(null);
+  public artists: ArtistByIdModel[] = [];
   public isLoading = true;
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     this.apiService
       .getFollowedArtists()
       .subscribe((artistList: FollowedArtistModel) => {
-        if (artistList.artists.items.length) {
-          this.artistInfo$.next(artistList.artists.items);
-        } else {
-          this.artistInfo$.next(null);
-        }
+        this.artists = artistList.artists.items;
         this.isLoading = false;
       });
   }
