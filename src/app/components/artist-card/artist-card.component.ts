58e1caac96c5362a83c209601039d8565e8c5ecc
Владимир <lordamncer1998electro@yandex.ru>
2022-03-16 20:42:37 +0400

    Merge branch 'master' into feature/13-SearchPage

--- src/app/components/artist-card/artist-card.component.ts
+++ src/app/components/artist-card/artist-card.component.ts
@@ -1,130 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { ArtistByIdModel } from "../../models/new-api-models/artist-by-id.model";
-import { TopTracksArtistByIdModel } from "../../models/new-api-models/top-tracks-artist-by-id.model";
+import { TopTracksModel } from "../../models/new-api-models/top-tracks-artist-by-id.model";
+import { ApiService } from "../../services/api.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "hb-artist-card",
   templateUrl: "./artist-card.component.html",
   styleUrls: ["./artist-card.component.less"]
 })
-export class ArtistCardComponent {
+export class ArtistCardComponent implements OnInit, OnDestroy {
+  @Input()
+  public artistInfo!: ArtistByIdModel;
   public isCard = true;
-  public artistInfo: ArtistByIdModel = {
-    followers: {
-      total: 2734564
-    },
-    genres: ["Rock", "Alternative Metal"],
-    id: "6XyY86QOPPrYVGvF9ch6wz",
-    name: "Linkin Park",
-    images: [
-      {
-        url: "https://i.scdn.co/image/ab6761610000e5eb34e5aa6afc1ba147bfbb0677"
-      }
-    ]
-  };
+  public topTracks: TopTracksModel[] = [];
+  public topTracksSearch$ = new Subscription();
 
-  public topTracks: TopTracksArtistByIdModel = {
-    tracks: [
-      {
-        id: "7uQ7e7nzbtyX87eIYHpj6Z",
-        name: "De Diepte",
-        preview_url:
-          "https://p.scdn.co/mp3-preview/1da2f7a06427f83820ef811485ded6423ac13b8a?cid=774b29d4f13844c495f206cafdad9c86",
-        track_number: 1,
-        album: {
-          id: "6XAKVt3CT7r1Zf0uiMWt7o",
-          artists: [],
-          images: [
-            {
-              url: "https://i.scdn.co/image/ab67616d0000b273518255a11ce96cc8de864f39"
-            }
-          ],
-          name: "De Diepte",
-          release_date: "2022-03-03",
-          total_tracks: 1
-        },
-        artists: [
-          {
-            id: "1zT9SWCzN45r7oVhy0VYLK",
-            name: "S10"
-          }
-        ]
-      },
-      {
-        id: "1HhNoOuqm1a5MXYEgAFl8o",
-        name: "Enemy",
-        preview_url:
-          "https://p.scdn.co/mp3-preview/bd0eaa32cd545406bc958e1a220fc36551e72cb0?cid=774b29d4f13844c495f206cafdad9c86",
-        track_number: 1,
-        album: {
-          id: "4fZIyJn2wKb51QPNnWYnqt",
-          artists: [],
-          images: [
-            {
-              url: "https://i.scdn.co/image/ab67616d0000b273d9b35d1c4d15c9de88b754a7"
-            }
-          ],
-          name: "De Diepte",
-          release_date: "2022-03-03",
-          total_tracks: 1
-        },
-        artists: [
-          {
-            id: "53XhwfbYqKCa1cC15pYq2q",
-            name: "Imagine Dragons"
-          }
-        ]
-      },
-      {
-        id: "1zB4vmk8tFRmM9UULNzbLBrrY",
-        name: "Thunder",
-        preview_url:
-          "https://p.scdn.co/mp3-preview/07e14c4e821174295fe50f03b33d98387570d1db?cid=774b29d4f13844c495f206cafdad9c86",
-        track_number: 1,
-        album: {
-          id: "4fZIyJn2wKb51QPNnWYnqt",
-          artists: [],
-          images: [
-            {
-              url: "https://i.scdn.co/image/ab67616d0000b2735675e83f707f1d7271e5cf8a"
-            }
-          ],
-          name: "Thunder",
-          release_date: "2022-03-03",
-          total_tracks: 1
-        },
-        artists: [
-          {
-            id: "1zT9SWCzN45r7oVhy0VYLK",
-            name: "Imagine Dragons"
-          }
-        ]
-      },
-      {
-        id: "7uQ7e7nzbtyX87eIYHpj6Z",
-        name: "De Diepte",
-        preview_url:
-          "https://p.scdn.co/mp3-preview/1da2f7a06427f83820ef811485ded6423ac13b8a?cid=774b29d4f13844c495f206cafdad9c86",
-        track_number: 1,
-        album: {
-          id: "6XAKVt3CT7r1Zf0uiMWt7o",
-          artists: [],
-          images: [
-            {
-              url: "https://i.scdn.co/image/ab67616d0000b273518255a11ce96cc8de864f39"
-            }
-          ],
-          name: "De Diepte",
-          release_date: "2022-03-03",
-          total_tracks: 1
-        },
-        artists: [
-          {
-            id: "1zT9SWCzN45r7oVhy0VYLK",
-            name: "S10"
-          }
-        ]
-      }
-    ]
-  };
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.topTracksSearch$ = this.api
+      .getArtistsTopTracks(this.artistInfo.id)
+      .subscribe(topTracks => {
+        this.topTracks = topTracks.tracks
+          .filter(topTrack => topTrack.preview_url !== null)
+          .slice(0, 4);
+      });
+  }
+
+  ngOnDestroy() {
+    this.topTracksSearch$.unsubscribe();
+  }
 }
--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { ArtistByIdModel } from "../../models/new-api-models/artist-by-id.model";
+import { Subscription } from "rxjs";
+import { SearchModel } from "../../models/new-api-models/search.model";
+import { ApiService } from "../../services/api.service";
+import { TrackById } from "../../models/new-api-models/track-by-id.model";
+import { SearchStateService } from "../../services/search-state.service";
 
 @Component({
   selector: "hb-search-page",
@@ -8,8 +14,29 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class SearchPageComponent {
   public key: string;
+  public artists: ArtistByIdModel[] = [];
+  public tracks: TrackById[] = [];
+  public searchResult$ = new Subscription();
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private api: ApiService,
+    private searchStateService: SearchStateService
+  ) {
     this.key = <string>this.activatedRoute.snapshot.queryParams["keyword"];
+    this.searchResult$ = this.searchStateService
+      .getSearchState()
+      .subscribe(key => {
+        this.api.searchForItem(key).subscribe((artists: SearchModel) => {
+          this.artists = artists.artists.items.filter(
+            artist => artist.followers.total > 100
+          );
+          this.tracks = artists.tracks.items;
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.searchResult$.unsubscribe();
   }
 }
