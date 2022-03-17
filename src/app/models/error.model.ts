e1537dc2f191058e70a9cb224a535c31be609429
AntoninaTrapsh <antrapsh@gmail.com>
2022-03-17 16:40:49 +0400

    fix

--- src/app/models/error.model.ts
+++ src/app/models/error.model.ts
@@ -11,35 +11,3 @@ export interface ErrorMessageModel {
 export interface ErrorModel {
   error: ErrorMessageModel;
 }
-
-// playlists, albums, saved tracks
-// {
-//   items: [
-//     {
-//       track: {
-//         информация о треке
-//       }
-//     }
-//   ]
-// }
-
-//search
-// {
-//   tracks: {
-//     items: [
-//       {
-//         информация о треке
-//       }
-//     ]
-//   }
-// }
-
-//top tracks artist
-
-// {
-//   tracks: [
-//     {
-//       информация о треке
-//     }
-//   ]
-// }
