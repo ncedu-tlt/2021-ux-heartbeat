11c83360abe3d4e4a4a0ee2323dcbb08370b02e4
Zanziabr <pavel.zanzibar.01@mail.ru>
2022-03-23 15:02:23 +0400

    pulseline

---
+++ src/app/components/pulseline/pulseline.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "hb-pulseline",
+  templateUrl: "./pulseline.component.html",
+  styleUrls: ["./pulseline.component.less"]
+})
+export class PulselineComponent {}
