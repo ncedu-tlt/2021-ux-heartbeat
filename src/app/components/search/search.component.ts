f4d1814f226cfcd50ba8355ab736b437f7cba8bd
Lordmanceroot <84021061+Lordmanceroot@users.noreply.github.com>
2022-03-23 13:29:07 +0400

    Feature/13 search page (#50)
    * Merge branch 'master' into feature/13-SearchPage
    
    * fix loader.component.less
    
    * feature/13-SearchPage
    
    * fix: delete filter from artist-card.component.ts && search-page.component.ts
    
    * fix: switched off autoplaying for search-page in track-list.component.ts
    
    * fixed styles with colors
    
    * added error handling "No search query" in search-page
    
    * fix: error handling in search-page
    
    * fix: explicit display of tracks that cannot be played and notification about it
    
    * minor fixes
    
    * made changes according to the tips
    
    Co-authored-by: Владимир <lordamncer1998electro@yandex.ru>

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { SearchStateService } from "../../services/search-state.service";
 
 @Component({
   selector: "hb-search",
@@ -9,10 +10,14 @@ import { Router } from "@angular/router";
 export class SearchComponent {
   public search = "";
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private searchStateService: SearchStateService
+  ) {}
 
   searchBtnClicked() {
     this.search = this.search.trim();
+    this.searchStateService.setSearchState(this.search);
     this.router.navigateByUrl(`/search?keyword=${this.search}`);
   }
 }
