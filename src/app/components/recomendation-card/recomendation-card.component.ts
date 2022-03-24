eb6f2ef5eff37031ee85688693deeb2e31a35ef4
LMikhail163 <81471455+LMikhail163@users.noreply.github.com>
2022-03-24 21:37:27 +0400

    Feature/43  recommendation page/genre page (#45)
    * RecommendationPage/GenrePage (not completed)
    
    * feature/43-RecommendationPage/GenrePage
    
    * not competed
    
    * Feature/42-player-modification (#46)
    
    * feature: player can work with playlists and control next/prev. track
    
    * add menu with urer's playlists
    
    * added possibility to add track into playlist
    
    * update branch
    
    * added possibility to check/add/delete tracks into/from favorite list
    
    * fix
    
    * mobile version: added possibility to add/delete/check tracks into/from favorite track list
    
    * added auto switch tracks in playlist
    
    * changed models
    
    * changed models №2
    
    * changed models №2
    
    * add error handlers and unsubscribe
    
    * add types
    
    * added possibility switch nex/pr. track on mobile version
    
    * fix resize
    
    * add context for tracks
    
    * fix styles
    
    * added search tracks model to track-list type
    
    * fix
    
    * fix #2
    
    * fix #3
    
    * fix #4
    
    * RecommendationPage
    
    * add loader to pages
    
    * finished
    
    * fix class "container"
    
    * Update artist-card.component.ts
    
    Co-authored-by: Antonina <82774971+AntoninaTrapsh@users.noreply.github.com>

---
+++ src/app/components/recomendation-card/recomendation-card.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, ViewEncapsulation } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ItemUserPlaylistModel } from "src/app/models/new-api-models/current-users-playlist.model";
+import { PlaylistTrackModel } from "src/app/models/new-api-models/top-tracks-artist-by-id.model";
+import { TrackLaunchContextEnum } from "src/app/models/track-launch-context.enum";
+import { ApiService } from "src/app/services/api.service";
+import { PlayerService } from "src/app/services/player.service";
+
+@Component({
+  selector: "hb-recomendation-card",
+  templateUrl: "./recomendation-card.component.html",
+  styleUrls: ["./recomendation-card.component.less"],
+  encapsulation: ViewEncapsulation.None
+})
+export class RecomendationCardComponent {
+  public trackInfo!: PlaylistTrackModel[];
+  public trackContext = TrackLaunchContextEnum.TOP_TRACKS;
+  public isCard = true;
+  public trackTime = 30;
+  public isLoading = true;
+  public info$ = new Subscription();
+
+  @Input() public recomendation!: ItemUserPlaylistModel;
+
+  constructor(
+    public apiService: ApiService,
+    private playerService: PlayerService
+  ) {}
+
+  ngOnInit(): void {
+    this.info$ = this.apiService
+      .getPlaylistTracks(this.recomendation.id)
+      .subscribe(topTracks => {
+        this.trackInfo = topTracks.items.slice(0, 4);
+        this.isLoading = false;
+      });
+  }
+
+  setListTrackIntoPlayer(): void {
+    this.playerService.trackList$.next(null);
+  }
+
+  ngOnDestroy(): void {
+    this.info$.unsubscribe();
+  }
+}
