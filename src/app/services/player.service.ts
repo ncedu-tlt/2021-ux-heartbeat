37421a50206234e1a76a39624d0f8602d75cbaa2
Lordmanceroot <84021061+Lordmanceroot@users.noreply.github.com>
2022-04-22 22:00:29 +0400

    Feature/68 improvement fixes #3 (#70)
    * show playlist via pushing button in the player added
    
    * patched track context in the recommendation-card
    
    * patched track context in the artist-page
    
    * patched track context in the artist-card
    
    * adjust api.service.ts according to current situation
    
    * patched track context in the search-page
    
    * fix: behavior scrollbar in opening tracklist when track is absence in player
    
    * fix: displaying tracks during mixing tracklist
    
    * merge master into this branch
    
    * fixed styles in playlists, when it is active, of a certain genre
    
    * fixed styles in featured artists on hover
    
    Co-authored-by: Владимир <lordamncer1998electro@yandex.ru>

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -50,7 +50,7 @@ export class PlayerService {
   public currentTrackInfo$ = new BehaviorSubject<Track | null>(null);
   private currentTrackNumber!: number;
   public trackList$ = new BehaviorSubject<TrackList | null>(null);
-  private shuffleTrackList!: TrackList;
+  public shuffleTrackList!: TrackList;
   public trackContext$ = new BehaviorSubject<
     string | TrackLaunchContextEnum | null | undefined
   >(null);
@@ -101,6 +101,9 @@ export class PlayerService {
         });
       this.isShuffle$.pipe(takeUntil(this.die$)).subscribe(isShuffle => {
         if (isShuffle && this.trackList$.getValue()) {
+          this.shuffleTrackList = JSON.parse(
+            JSON.stringify(this.trackList$.getValue())
+          ) as TrackList;
           this.mixCurrentTrackList();
         }
         if (!isShuffle && this.trackList$.getValue()?.items) {
@@ -252,7 +255,7 @@ export class PlayerService {
       this.shuffleTrackList.items[0].track
     ] = [
       this.shuffleTrackList.items[0].track,
-      this.currentTrackInfo$.getValue() as Track
+      this.shuffleTrackList.items[this.currentTrackNumber].track
     ];
     this.currentTrackNumber = 0;
     for (let i = this.shuffleTrackList.items.length - 1; i > 1; i--) {
