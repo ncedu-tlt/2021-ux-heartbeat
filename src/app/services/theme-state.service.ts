84c8fb44b8c715b9eee80e61bb25090b156b4323
Zanziabr <--global>
2022-05-08 14:35:11 +0400

    darktheme

--- src/app/services/theme-state.service.ts
+++ src/app/services/theme-state.service.ts
@@ -5,6 +5,11 @@ import { Injectable } from "@angular/core";
 })
 export class ThemeStateService {
   private isDarkTheme = false;
+  ThemeStateService(isDarkTheme: boolean) {
+    if (localStorage.getItem("darkTheme")) {
+      this.isDarkTheme = isDarkTheme;
+    } else this.isDarkTheme = false;
+  }
 
   public getIsDarkTheme(): boolean {
     return this.isDarkTheme;
@@ -12,6 +17,7 @@ export class ThemeStateService {
 
   public setIsDarkTheme(isDarkTheme: boolean): void {
     this.isDarkTheme = isDarkTheme;
+    localStorage.setItem("darkTheme", JSON.stringify(isDarkTheme));
     document.body.style.background = isDarkTheme
       ? "linear-gradient(252.82deg, rgba(54, 66, 109) 72.05%, rgba(12, 14, 24, 0.7) 100%) no-repeat fixed"
       : "#FFFFFF";
