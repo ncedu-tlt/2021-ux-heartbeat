b336e48a96a5aa35ea5bb921543b6b05e4f26bb1
Владимир <lordamncer1998electro@yandex.ru>
2022-05-17 18:08:52 +0400

    fix: saving theme-state for localStorage

--- src/app/services/theme-state.service.ts
+++ src/app/services/theme-state.service.ts
@@ -6,19 +6,16 @@ import { Injectable } from "@angular/core";
 export class ThemeStateService {
   private isDarkTheme = false;
 
-  ThemeStateService() {
-    this.isDarkTheme = localStorage.getItem("isDarkTheme") === "true";
-  }
-
   public getIsDarkTheme(): boolean {
+    this.isDarkTheme = localStorage.getItem("isDarkTheme") === "true";
+    document.body.style.background = this.isDarkTheme
+      ? "linear-gradient(252.82deg, rgba(54, 66, 109) 72.05%, rgba(12, 14, 24, 0.7) 100%) no-repeat fixed"
+      : "#FFFFFF";
     return this.isDarkTheme;
   }
 
   public setIsDarkTheme(isDarkTheme: boolean): void {
     this.isDarkTheme = isDarkTheme;
     localStorage.setItem("isDarkTheme", JSON.stringify(this.isDarkTheme));
-    document.body.style.background = isDarkTheme
-      ? "linear-gradient(252.82deg, rgba(54, 66, 109) 72.05%, rgba(12, 14, 24, 0.7) 100%) no-repeat fixed"
-      : "#FFFFFF";
   }
 }
